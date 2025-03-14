08b3cfaf3ef0bbf4f6ad36c26d25c9670a822f53
sidharthify <wednisegit@gmail.com>
2025-03-14 18:55:46 +0000

    pantah: Fix invalid PRODUCT_BUILD_PROP_OVERRIDES keys Signed-off-by: sidharthify <wednisegit@gmail.com>

--- statix_panther.mk
+++ statix_panther.mk
@@ -1,26 +1,25 @@
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2280
-TARGET_SCREEN_WIDTH := 1080
+# Boot animation  
+TARGET_SCREEN_HEIGHT := 2280  
+TARGET_SCREEN_WIDTH := 1080  
 
-# Inherit some common StatiX stuff.
-$(call inherit-product, vendor/statix/config/common.mk)
-$(call inherit-product, vendor/statix/config/gsm.mk)
+# Inherit some common StatiX stuff.  
+$(call inherit-product, vendor/statix/config/common.mk)  
+$(call inherit-product, vendor/statix/config/gsm.mk)  
 
-# Inherit device configuration
-$(call inherit-product, device/google/pantah/aosp_panther.mk)
+# Inherit device configuration  
+$(call inherit-product, device/google/pantah/aosp_panther.mk)  
 
-# Parts
-$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)
-$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)
+# Parts  
+$(call inherit-product-if-exists, vendor/google/pixelparts/pixelparts.mk)  
+$(call inherit-product-if-exists, vendor/google/pixelparts/powershare/device.mk)  
 
-## Device identifier. This must come after all inclusions
-PRODUCT_NAME := statix_panther
-PRODUCT_MODEL := Pixel 7
-PRODUCT_BRAND := google
-PRODUCT_MANUFACTURER := Google
+## Device identifier. This must come after all inclusions  
+PRODUCT_NAME := statix_panther  
+PRODUCT_MODEL := Pixel 7  
+PRODUCT_BRAND := google  
+PRODUCT_MANUFACTURER := Google  
 
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRODUCT_NAME=panther \
-    PRIVATE_BUILD_DESC="panther-user 15 AP4A.250205.002 12821496 release-keys"
-
-BUILD_FINGERPRINT := $(PRODUCT_OVERRIDE_FINGEPRINT)
+    BuildDesc="panther-user 15 AP4A.250205.002 12821496 release-keys" \
+    BuildFingerprint=google/panther/panther:15/AP4A.250205.002/12821496:user/release-keys \
+    DeviceProduct=$(DEVICE_CODENAME)
